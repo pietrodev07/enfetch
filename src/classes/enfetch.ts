57bbf30d1d2b743916177f83053bce747204ed47
pietrodev07 <pietro.dev.07@gmail.com>
2024-04-30 15:28:50 +0200

    feat: add get method in enfetch class

--- src/classes/enfetch.ts
+++ src/classes/enfetch.ts
@@ -1,9 +1,18 @@
-import { DefaultRequestConfig } from "../types";
+import { DefaultRequestConfig, RequestSchema } from "../types";
+import { request } from "../utils/request";
 
 export class enfetch {
   protected defaultConfig: DefaultRequestConfig;
 
   public constructor(config: DefaultRequestConfig = {}) {
     this.defaultConfig = { ...config };
+  }
+
+  public async get<D>(url: string, options?: RequestSchema<null>) {
+    return request<null, D>(url, {
+      method: "GET",
+      ...options,
+      ...this.defaultConfig,
+    });
   }
 }
